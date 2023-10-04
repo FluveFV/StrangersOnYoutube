521a0ee21edfb1bf831a2804cd11b1252b8077f6
FluveV <davide.vandelli@studenti.unitn.it>
2023-10-04 15:55:41 +0200

    upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import json
+import pandas as pd
+import pprint
+import numpy as np
+file_name = "sample_for_groundtruth.csv"
+
+class annotation:
+
+    # at least four attributes:
+    # 0 loads the dataframe with comments from the same folder this class is located in
+    # 1 checks the state of progress is made it's uploaded and won't be lost.
+    # 2 takes prints a comment and takes the annotation input
+    # 3 takes iterates over the remaining not annotated comments and calles each time
+    # the function number 2
+    # 4 saves the progress made.
+    def __init__(self):
+        self.dataloader = pd.read_csv(file_name)
+        self.dataloader.rename(columns={'Unnamed: 0': 'Original indexes', '0': 'User ID'}, inplace=True)
+        print("Data was loaded")
+        print(self.dataloader.head(5))
+
+    def progress(self):
+        2
+
+    def ground_truthing(text):
+        pp = pprint.PrettyPrinter(width=64, depth=1)
+        pp.pprint(text)
+        inp = str(input("Evaluation:\t\t"))
+        if inp not in ["0", "1", "2"]:
+            print("Please insert a valid evaluation between 0, 1, 2, or \n press 'exit' to quit.")
+            if str(input("")) == "exit":
+                return float('NaN')
+            inp = int(ground_truthing(text))
+        return inp
+
+    ground_truthing("test " * 6 * 14)
+
+
+annotation()
+
+
+
+
